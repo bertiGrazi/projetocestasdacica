7aa80728b72fae699064bd0c58858c4c6511da26
Grazi Berti <limaberti.grazi@gmail.com>
2026-07-16 20:29:21 -0300

    feat: create mobile home page

--- src/components/ui/primary-button.tsx
+++ src/components/ui/primary-button.tsx
@@ -7,10 +7,17 @@ export function PrimaryButton({
 }: ButtonProps) {
   return (
     <Button
+      type="button"
       className={`
+        mt-10
+        w-full
+        max-w-sm
+        rounded-2xl
+        py-6
         bg-brand-primary
         text-white
         hover:bg-brand-primary/90
+        text-2xl
         ${className}
       `}
       {...props}
