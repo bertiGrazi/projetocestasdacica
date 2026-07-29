d59dbdf793a97f54b4684c0373fa219077547847
Grazi Berti <limaberti.grazi@gmail.com>
2026-07-29 20:24:18 -0300

    feat: add dynamic occasion page

--- src/app/occasions/[slug]/page.tsx
+++ src/app/occasions/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { occasions } from "@/src/data/occasions";
+
 type OccassionsPageProps = {
   params: Promise<{
     slug: string;
@@ -7,11 +9,29 @@ type OccassionsPageProps = {
 export default async function OccassionsPage({ params }: OccassionsPageProps) {
   const { slug } = await params;
 
+  const occasion = occasions.find(
+    (occasion) => occasion.id === slug
+  );
+
+  if (!occasion) {
+    return (
+      <main className="min-h-screen px-4 py-8">
+        <h1 className="text-3xl font-bold">Ocasião não encontrada</h1>
+        <p className="mt-2 text-gray-600">
+          A ocasião que você está procurando não foi encontrada.
+        </p>
+      </main>
+    )
+  }
+
   return (
     <main className="min-h-screen px-4 py-8">
       <h1 className="text-3xl font-bold">
-        Ocasião: {slug}
+        {occasion?.emoji} {occasion?.title}
       </h1>
+      <p className="mt-2 text-gray-600">
+        {occasion?.description}
+      </p>
     </main>
   )
 }
